6a6632b5555dde71ee6170a56f04bf90c3139835
tiago <tiago@LAPTOP-K9A0T2FJ.broadband>
2023-02-21 23:08:06 +0000

    Completed exercises 1 + 2

--- src/scw3/ArrayOfNumbers.java
+++ src/scw3/ArrayOfNumbers.java
@@ -36,4 +36,74 @@ public class ArrayOfNumbers {
         }
         return max;
     }
+    
+    public String equal(int num1, int num2){
+        String result = "";
+        int count1 = 0, count2 = 0;
+        for(int i=0; i<numlist.length; i++){
+            if(numlist[i] == num1){
+                count1++;
+            }
+            if(numlist[i] == num2){
+                count2++;
+            }
+        }
+        if(count1 == 0 && count2 == 0){
+            result = "No matches found.";
+        }
+        
+        if(count1 > 0){
+            result += "The first number is a match. It exists " + count1 + " times\n";
+        }
+        
+        if(count2 > 0){
+            result += "The second number is a match. It exists " + count2 + " times\n";
+        }
+ 
+        return result;
+    }
+    
+    public int gcd(int a, int b){
+        do{
+            if(a < b){
+                b = b - a;
+            }
+            else{
+                a = a - b;
+            }    
+        }while(a != b);
+        return a;
+    }
+    
+    public int count(){
+        return numlist.length;
+    }
+    
+    public int sum(){
+        int sum = 0;
+        for(int i=0; i < numlist.length; i++){
+            sum = sum + numlist[i];
+        }
+        return sum;
+    }
+    
+    public int avg(){
+        int avg = 0;
+        for(int i=0; i < numlist.length; i++){
+            avg = avg + numlist[i];
+        }
+        return avg/numlist.length;
+    }
+    
+    public void scalarMultiply(int x){
+        for(int i=0; i < numlist.length; i++){
+            numlist[i] = numlist[i] * x;
+        }
+    }
+    
+    public void addConstant(int x){
+        for(int i=0; i < numlist.length; i++){
+            numlist[i] = numlist[i] + x;
+        }
+    }
 }
--- src/scw3/SCW3.java
+++ src/scw3/SCW3.java
@@ -25,26 +25,76 @@ public class SCW3 {
             System.out.println("1 - Return a value \n"
                              + "2 - Set a value \n" 
                              + "3 - Return max value of the array\n"
-                             + "0 - Exit\n");
+                             + "4 - Two number search on array\n"
+                             + "5 - GCD between two numbers\n"
+                             + "6 - Count the existing numbers in the list\n"
+                             + "7 - Sum all values in array\n"
+                             + "8 - Average of all values in array\n"
+                             + "9 - Multiply each array element with a given number\n"
+                             + "10 - Add each array element with a given number\n"
+                             + "0 - Exit");
             op = in.nextInt();
             switch(op){
+                
                 case 1:
-                    System.out.println("Please insert a value:\n");
+                    System.out.println("Please insert a value:");
                     num1 = in.nextInt();
                     System.out.println(data.item(num1));
-                break;
+                    break;
+                
                 case 2:
-                    System.out.println("Please insert the array reference:\n");
+                    System.out.println("Please insert the array reference:");
                     num1 = in.nextInt();
-                    System.out.println("Please insert a value:\n");
+                    System.out.println("Please insert a value:");
                     num2 = in.nextInt();
                     if (data.setitem(num1, num2)){
-                        System.out.println("Number changed!\n");
+                        System.out.println("Number changed!");
                     }
-                break;
+                    break;
+                
                 case 3:
-                    System.out.println(data.max());
-                break;
+                    System.out.println("Max number is "+data.max());
+                    break;
+                
+                case 4:
+                    System.out.println("Please insert a number to compare:");
+                    num1 = in.nextInt();
+                    System.out.println("Please insert another number to compare:");
+                    num2 = in.nextInt();
+                    System.out.println(data.equal(num1, num2));
+                    break;
+                
+                case 5:
+                    System.out.println("Please insert a number to compare:");
+                    num1 = in.nextInt();
+                    System.out.println("Please insert another number to compare:");
+                    num2 = in.nextInt();
+                    System.out.println("The GDC between " + num1 + " and " + num2 + " is " + data.gcd(num1, num2));
+                    break;
+                
+                case 6:
+                    System.out.println("The array size is: " + data.count());
+                    break;
+                
+                case 7:
+                    System.out.println("The array total value is: " + data.sum());
+                    break;
+                    
+                case 8:
+                    System.out.println("The array average value is: " + data.avg());
+                    break;
+                    
+                case 9:
+                    System.out.println("Please insert a number to multiply with each element in array:");
+                    num1 = in.nextInt();
+                    data.scalarMultiply(num1);
+                    break;
+                    
+                case 10:
+                    System.out.println("Please insert a number to add to each element in array:");
+                    num1 = in.nextInt();
+                    data.addConstant(num1);
+                    break;
             }
         }while(op!=0);
     }
